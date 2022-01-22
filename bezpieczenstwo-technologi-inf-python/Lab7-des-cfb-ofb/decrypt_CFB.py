96d6c618190261ccf0e868efc8f1da94726fc023
Teśluk Mateusz <s59049@student.ath.edu.pl>
2022-01-22 15:08:10 +0100

    BTI Des cfb ofb

---
+++ bezpieczenstwo-technologi-inf-python/Lab7-des-cfb-ofb/decrypt_CFB.py
@@ -0,0 +1,20 @@
+import sys
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+
+keyword = "hello123"
+try:
+    keyword = sys.argv[1]
+except IndexError:
+    pass
+keyword = keyword.replace(" ", "").upper()
+
+with open("out.txt", "rb") as file:
+    input_text = file.read()
+
+des = DES.new(keyword.encode('utf8'), DES.MODE_CFB, b"8bytes12")
+res = des.decrypt(input_text)
+output_text = unpad(res, DES.block_size)
+
+with open("out_in.txt", "wb") as file:
+    file.write(output_text)
---
+++ bezpieczenstwo-technologi-inf-python/Lab7-des-cfb-ofb/encrypt_OFB.py
@@ -0,0 +1,21 @@
+import sys
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+
+
+keyword = "hello123"
+try:
+    keyword = sys.argv[1]
+except IndexError:
+    pass
+keyword = keyword.replace(" ", "").upper()
+
+with open("in.txt") as file:
+    input_text = file.read().replace(" ", "").upper()
+
+des = DES.new(keyword.encode('utf8'), DES.MODE_OFB, b"8bytes12")
+padded_text = pad(input_text.encode('utf8'), DES.block_size)
+output_text = des.encrypt(padded_text)
+
+with open("out.txt", "wb") as file:
+    file.write(output_text)
